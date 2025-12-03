802f0338387d9c74242663833f85ed943cf2b889
Leon Klute <leonklute@live.nl>
2025-12-03 09:01:38 +0100

    d2p2

--- d02/p2.java
+++ d02/p2.java
@@ -1,5 +1,5 @@
 void main() throws Exception {
-    var ranges = Files.lines(Path.of("test.txt")).findFirst().get().split(",");
+    var ranges = Files.lines(Path.of("input.txt")).findFirst().get().split(",");
     long total = Arrays.stream(ranges)
         .map(r -> r.split("-"))
         .mapToLong(r -> invalidCount(r[0], r[1]))
@@ -8,31 +8,38 @@ void main() throws Exception {
 }
 
 static long invalidCount(String start, String end) {
-    long total = 0;
-    for (int i = 1; i <= (end.length()+1)/2; i++) {
-        total += invalidCount(start, end, i);
+    Set<String> invalids = new HashSet<>();
+    if(start.length() != end.length()){
+        assert end.length() - start.length() == 1;
+        StringBuilder startEnd = new StringBuilder();
+        for (int i = 0; i < start.length(); i++) {
+            startEnd.append('9');
+        }
+        StringBuilder endStart = new StringBuilder("1");
+        for (int i = 0; i < end.length()-1; i++) {
+            endStart.append('0');
+        }
+        IO.println("split: " + start + " " +startEnd.toString() + " " + endStart.toString() + " " + end);
+        return invalidCount(start, startEnd.toString()) + invalidCount(endStart.toString(), end);
     }
-    return total;
+    for (int i = 1; i <= start.length()/2; i++) {
+        if(start.length() % i != 0){
+            continue;
+        }
+        invalidCount(start, end, i, invalids);
+    }
+    return invalids.stream().mapToLong(s -> Long.parseLong(s)).sum();
 }
 
-static long invalidCount(String start, String end, int seqSize) {
+static Set<String> invalidCount(String start, String end, int seqSize, Set<String> invalids) {
     IO.println("invalidCount " + start + ", " + end + ", " + seqSize);
     long total = 0;
     int startSize = start.length();
     int endSize = end.length();
     IO.println("startSize" + startSize);
     IO.println("endSize" + endSize);
-    if(seqSize > (endSize+1)/2) {
-        return 0;
-    }
-    long halfStart;
-    long halfEnd;
-    try{
-        halfStart = Long.parseLong(start.substring(0, startSize-seqSize));
-    } catch(Exception e){
-        halfStart = 0;
-    }
-    halfEnd = Long.parseLong(end.substring(0, seqSize));
+    long halfStart = Long.parseLong(start.substring(0, seqSize));
+    long halfEnd = Long.parseLong(end.substring(0, seqSize));
     IO.println("halfs: " + halfStart + ", " + halfEnd);
     Set<String> candidates = LongStream.range(halfStart, halfEnd+1)
         .mapToObj(l -> {
@@ -50,10 +57,9 @@ static long invalidCount(String start, String end, int seqSize) {
         // IO.println("candidate: "+candidate);
         long target = Long.parseLong(candidate);
         if(target >= startL && target <= endL){
-            total += target;
+            invalids.add(candidate);
         }
-        // IO.readln();
     }
     IO.println("return: "+total);
-    return total;
+    return invalids;
 }
